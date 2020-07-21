91cf05497a5e199f26dd20b27192030eee3f44b5
jerrylouis33161 <jloves101@gmail.com>
2020-07-21 17:46:17 -0400

    assignment 2

---
+++ src/org/launchcode/techjobs_oo/JobTest.java
@@ -0,0 +1,13 @@
+package org.launchcode.techjobs_oo;
+
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+    @Test
+    public void testSettingJobId(){
+        assertEquals(1,);
+    }
+}
